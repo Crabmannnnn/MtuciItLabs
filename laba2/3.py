d24a52f393ba260f5d4a7ac10bbbca95ff52bc20
Crabmannnnn <t.gagasik@gmail.com>
2025-10-17 08:10:53 +0300

    Create 3.py

---
+++ laba2/3.py
@@ -0,0 +1,16 @@
+def is_prime(number):
+    for x in range(2, number+1):
+        if x<number and number%x==0:
+            return False
+        else:
+            ++x
+        return True
+number=input("Введите число")
+try:
+    number=int(number)
+    if is_prime(number)==True:
+        print("Это простое число")
+    else:
+        print("Это не простое число")
+except ValueError:
+    print("Это не число")
