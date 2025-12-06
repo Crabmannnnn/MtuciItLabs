ea8afb58705e710fd346e946bb0cc84d209e0922
Crabmannnnn <t.gagasik@gmail.com>
2025-12-06 08:12:32 +0300

    Update task1.py

--- laba6/task1.py
+++ laba6/task1.py
@@ -9,7 +9,7 @@ class UserAccount:
     def __init__(self,username,email,password):
         self.username=username
         self.email=email
-        self.password=password
+        self.__password=password
     def set_password(self,new_password):
          self.password=new_password
     def check_password(self,password):
